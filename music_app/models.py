bf9ae48a2e072aff08bffa765ea828ea02994482
GianniADavies <giannidavies105@gmail.com>
2022-11-19 13:21:54 -0500

    Final updates

--- music_app/models.py
+++ music_app/models.py
@@ -1,31 +1,40 @@
 from django.db import models
 
-# Create your models here.
 class Artist(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=False)
-    age = models.CharField(max_length=200, null=True)
-    nationality = models.CharField(max_length=200)
-    website = models.CharField(max_length=100)
-    height = models.CharField(max_length=100)
-    label = models.CharField(max_length=200)
-    image_url = models.CharField(max_length=200,null=True)
+    name = models.CharField('',max_length=100, null=False)
+    age = models.IntegerField('',null=True)
+    nationality = models.CharField('',max_length=200)
+    website = models.CharField('',max_length=100)
+    label = models.CharField('',max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Artist'
+        verbose_name_plural = 'Artists'
 
 
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
     genre = models.CharField(max_length=60)
     title = models.CharField(max_length=100)
-    release_date = models.DateField(null=True,auto_now=False)
+    release_year = models.IntegerField(null=True)
     album = models.CharField(max_length=80, null=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
 
+    class Meta:
+        verbose_name = "Song"
+        verbose_name_plural = "Songs"
 
-    @property
-    def artistName(self):
-        return self.artist.name
+@property
+def artistName(self):
+    return self.artist.name
 
-    @property
-    def artistId(self):
-        return self.artist.id
+@property
+def artistId(self):
+    return self.artist.id
--- musicgenie/settings.py
+++ musicgenie/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'music_app.apps.MusicAppConfig',
+    "crispy_forms",
+    "crispy_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'musicgenie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
